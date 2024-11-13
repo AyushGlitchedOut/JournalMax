51f5243c5f35990727e8fdda2e23441529b160f4
Ayush Gupta <ayushgupta.svma@gmail.com>
2024-11-13 11:50:45 +0530

    Editor Page in Progress

--- android/app/src/main/kotlin/com/example/journalmax/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.journalmax
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/Pages/EditorPage.dart
+++ lib/Pages/EditorPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:journalmax/Widgets/XAppBar.dart';
 import 'package:journalmax/Widgets/XDrawer.dart';
+import 'package:journalmax/Widgets/XDropDown.dart';
+import 'package:journalmax/Widgets/XIconLabelButton.dart';
+import 'package:journalmax/Widgets/XLabel.dart';
 
 class EditorPage extends StatelessWidget {
   const EditorPage({super.key});
@@ -18,6 +21,22 @@ class EditorPage extends StatelessWidget {
         currentPage: "editor",
       ),
       backgroundColor: colors.surface,
+      body: Column(
+        children: [
+          XDropdown(label: "Mood", list: [
+            "happy",
+            "sad",
+            "angry",
+            "excited",
+            "mundane",
+            "surprised",
+            "frustrated",
+            "doubtful",
+            "anxious"
+          ]),
+          XIconLabelButton(icon: Icons.image_outlined, label: "Add Multimedia")
+        ],
+      ),
     );
   }
 }
