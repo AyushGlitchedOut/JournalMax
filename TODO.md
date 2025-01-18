5c82346140b91a2cfcf9ee9e3c4d0bd475044bfc
Ayush Gupta <ayushgupta.svma@gmail.com>
2025-01-18 16:53:11 +0530

    Finally animation of snackBar complete

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-1. Add error handlign with snackbars everywhere (Including sending reports) (Make a reusable error handling widget and put it wherever there is DB operation, api calling or something like that)
+1. Add error handling with snackbars everywhere (Including sending reports) (Make a reusable error handling widget and put it wherever there is DB operation, api calling or something like that)
 2. *** DONE *** Opening websites in the settings page for source code, author etc. (The github page for me, source code and a simple licenses page)
 3. Adding multimedia options (using apis and custom screens) **Location Done**
 4. Google Drive integration
@@ -12,13 +12,14 @@
 13. **DONE** Add the AppIcon in the App Drawer
 14. **DONE** Add a gradient background to the Drawer
 15. Export the DataBase as a folder in downloads folder with each entry as a different file
-16. Make the snackBar fade In and Out
+16. **DONE** Make the snackBar fade In and Out
 17. **DONE** Update everything to a single naming convention, preferabbly to lowerCamelCase for consistency
 18. **DONE** Consistency in font sizes and border-radius, color etc. for interface
 19. **DONE** Fix themeing and theme consistency issues
 20. Testing with profiling tools and devtools debugger along with security
 21. **DONE** Wipe and delete images upon wiping and deleting entries corresponding to them
-22. Move SaveImagesToFile into editor page upon saving the entry for security reasons
+22. **DONE** Move SaveImagesToFile into editor page upon saving the entry for security reasons
 22. Ability to view all the entry data inlcluding multimedia in ViewerPage
 23. Change Navigation to be better and fix the autosave bug where the returned to page doesnt re-render unless explicitly told from editor page
 24. A bug where a entry needs to be deleted two times for the parent widget to display the corrected list
+25. Put subtle onclick animations on all buttons and interactables
--- lib/widgets/XSnackBar.dart
+++ lib/widgets/XSnackBar.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 void showSnackBar(String message, BuildContext context) {
   final ColorScheme colors = Theme.of(context).colorScheme;
-  OverlayEntry? overLayEntry;
+  OverlayEntry? XSnackBar;
+  double snackBarOpacity = 0.0;
 
   //Custom SnackBar using Overlay
-  overLayEntry = OverlayEntry(builder: (BuildContext context) {
+  XSnackBar = OverlayEntry(builder: (BuildContext context) {
     return SafeArea(
       child: Align(
         alignment: Alignment.bottomCenter,
@@ -13,64 +14,68 @@ void showSnackBar(String message, BuildContext context) {
           padding: EdgeInsets.symmetric(
               vertical: MediaQuery.of(context).size.width * 0.22,
               horizontal: 10.0),
-          child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      stops: const [0.9, 1],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [colors.onPrimary, colors.tertiary]),
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                        color: colors.primary,
-                        offset: const Offset(-1.5, -1.5)),
-                    BoxShadow(
-                        color: colors.outline, offset: const Offset(1.5, 1.5))
-                  ],
-                  border: Border.all(color: colors.outline),
-                  color: colors.onPrimary),
-              height: MediaQuery.of(context).size.height * 0.08,
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      const SizedBox(
-                        width: 10.0,
-                      ),
-                      Center(
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                            color: colors.secondary,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15.0,
-                          ),
-                          child: Text(
-                            message,
+          child: AnimatedOpacity(
+            duration: Duration(milliseconds: 500),
+            opacity: snackBarOpacity,
+            child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        stops: const [0.9, 1],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [colors.onPrimary, colors.tertiary]),
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                          color: colors.primary,
+                          offset: const Offset(-1.5, -1.5)),
+                      BoxShadow(
+                          color: colors.outline, offset: const Offset(1.5, 1.5))
+                    ],
+                    border: Border.all(color: colors.outline),
+                    color: colors.onPrimary),
+                height: MediaQuery.of(context).size.height * 0.08,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 10.0,
+                        ),
+                        Center(
+                          child: DefaultTextStyle(
                             style: TextStyle(
                               color: colors.secondary,
                               fontWeight: FontWeight.bold,
-                              fontSize: 17.0,
+                              fontSize: 15.0,
+                            ),
+                            child: Text(
+                              message,
+                              style: TextStyle(
+                                color: colors.secondary,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17.0,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        overLayEntry?.remove();
-                        overLayEntry?.dispose();
-                        overLayEntry = null;
-                      },
-                      icon: Icon(
-                        Icons.close_rounded,
-                        color: colors.primary,
-                      ))
-                ],
-              )),
+                      ],
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          XSnackBar?.remove();
+                          XSnackBar?.dispose();
+                          XSnackBar = null;
+                        },
+                        icon: Icon(
+                          Icons.close_rounded,
+                          color: colors.primary,
+                        ))
+                  ],
+                )),
+          ),
         ),
       ),
     );
@@ -79,15 +84,27 @@ void showSnackBar(String message, BuildContext context) {
   //insert the overlay
   Overlay.of(
     context,
-  ).insert(overLayEntry ??
+  ).insert(XSnackBar ??
       OverlayEntry(builder: (BuildContext context) {
         return Container();
       }));
 
-  Future.delayed(const Duration(milliseconds: 1500), () {
-    overLayEntry?.remove();
-    overLayEntry?.dispose();
-    overLayEntry = null;
+  //Fade In
+  Future.delayed(Duration(milliseconds: 100), () {
+    snackBarOpacity = 1.0; // Make it visible
+    XSnackBar?.markNeedsBuild(); // Rebuild the overlay to apply opacity
+  });
+  //Fade Out
+  Future.delayed(Duration(milliseconds: 1500), () {
+    snackBarOpacity = 0.0;
+    XSnackBar?.markNeedsBuild();
+  });
+
+  //Delete it
+  Future.delayed(const Duration(milliseconds: 2000), () {
+    XSnackBar?.remove();
+    XSnackBar?.dispose();
+    XSnackBar = null;
   });
   return;
 }
